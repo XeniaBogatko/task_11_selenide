d6dbb27ac929420b098e79be2ac08c2aabc550c6
Xenia Bogatko <bogatkok@gmail.com>
2022-11-14 22:38:18 +0100

    should be visible added

--- src/test/java/github/SelenideSearch.java
+++ src/test/java/github/SelenideSearch.java
@@ -3,7 +3,9 @@ package github;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideSearch {
 
@@ -11,13 +13,14 @@ public class SelenideSearch {
     void shouldFindJunit5Example() {
         open("https://github.com");
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-        $$(".repo-list li").first().$("a").click();
+        $(".repo-list li").$("a").click();
         $("#wiki-tab").click();
 
         $(".wiki-more-pages-link").$("button").click();
         $("[data-filterable-for='wiki-pages-filter']").shouldHave(text("SoftAssertions"));
 
         $("a[href$='SoftAssertions']").click();
-        $("#user-content-3-using-junit5-extend-test-class").$("pre").isDisplayed();
+        $("#user-content-3-using-junit5-extend-test-class").closest("h4")
+                .sibling(0).$("pre").shouldBe(visible);
     }
 }
