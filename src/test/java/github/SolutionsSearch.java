106d76e153e59267210b98a990914d3c36ead8c5
Xenia Bogatko <bogatkok@gmail.com>
2022-11-17 14:19:00 +0100

    Enterprise check added

--- src/test/java/github/SolutionsSearch.java
+++ src/test/java/github/SolutionsSearch.java
@@ -1,14 +1,14 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SolutionsSearch {
     @Test
-    void searchSolutionsEnterprise(){
+    void searchSolutionsEnterprise() {
         open("https://github.com");
         $$("button").findBy(text("Solutions")).hover();
         $$(".HeaderMenu-dropdown").findBy(visible).$$("ul li a").findBy(text("Enterprise")).click();
